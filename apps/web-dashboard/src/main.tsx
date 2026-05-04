a12b29c09a4c9a9a263709d7a7cbb7d96a63b5a3
Adebayo Muhammed Olosasa <95596908+Mosas2000@users.noreply.github.com>
2026-05-04 19:43:06 +0100

    Fix/web dashboard auth shell (#390)

--- apps/web-dashboard/src/main.tsx
+++ apps/web-dashboard/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
-import { TableDensityProvider } from './contexts/TableDensityContext';
-import './index.css';
+import { DashboardApp } from './router';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <TableDensityProvider>
-      <App />
-    </TableDensityProvider>
+    <DashboardApp />
   </React.StrictMode>
 );
